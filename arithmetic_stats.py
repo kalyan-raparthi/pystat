6f2bb0eb3c7eac48ac65706f628adfd2ea6a50d0
qb <83409253+kalyan-raparthi@users.noreply.github.com>
2025-01-21 14:29:03 +0530

    added arithmetic_stats.py

---
+++ arithmetic_stats.py
@@ -0,0 +1,9 @@
+# AUTHOUR: kalyan raparthi / [ qb ], kalyan.raparthi@hotmail.com, GitHub: kalyan-raparthi
+
+# AVERAGE IN LIST
+def avg(list_input):
+    return sum(list_input) / len(list_input)
+    
+# test avg(list_input)
+# print(avg([1, 2, 5, 2, 5]))
+    
