7c0f4203fc8736fc21a2f0ae2489b6dfa91491b7
qb <83409253+kalyan-raparthi@users.noreply.github.com>
2025-02-10 22:25:52 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
-# probability funtion
+def prob(arr, index):
+    recur = 0
+    for i in arr:
+        if (arr[index] == i):
+            recur += 1
+    return ((recur / len(arr)) * 100)
+  
 
-def probability(arr, index):
-  recur = 0
-  for i in arr:
-    if (arr[index] == i):
-      recur += 1
-  return (recur / len(arr))
-
+# usage of probability funtion
+# arr = [1, 2, 3, 4, 5, 6]
+# print(prob(arr, arr.index(2)))
